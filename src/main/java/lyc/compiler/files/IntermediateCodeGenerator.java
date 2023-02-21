70a2af5dfe4e6149852ee9909fee75475a5b5f43
Martin <martin.pfe@gmail.com>
2023-02-21 13:26:14 -0300

    Implementacion de tercetos

--- src/main/java/lyc/compiler/files/IntermediateCodeGenerator.java
+++ src/main/java/lyc/compiler/files/IntermediateCodeGenerator.java
@@ -2,11 +2,71 @@ package lyc.compiler.files;
 
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class IntermediateCodeGenerator implements FileGenerator {
 
+    public static class Triple {
+        public Integer position;
+        public String opcode;
+        public String src;
+        public String dest;
+    
+        public Triple(String opcode, String src, String dest) {
+            this.position = triples.size();
+            this.opcode = opcode;
+            this.src = src;
+            this.dest = dest;
+        }
+    }
+
+    public static int getTripleCount() {
+        return triples.size();
+    }
+
+    //Update the triple
+    public static void updateTriple(Integer position, String src, String dest) {
+        triples.get(position).src = src;
+        triples.get(position).dest = dest;
+    }
+
+    //Update the triple
+    public static void updateTriple(Integer position, String src) {
+        triples.get(position).src = src;
+    }
+
+    //Triples
+    public static List<Triple> triples = new ArrayList<>();
+
+    public static Integer addTriple(Object opcode, Object src, Object dest) {
+        Triple newTriple = new Triple(opcode.toString(), src.toString(), dest.toString());
+
+        triples.add(newTriple);
+
+        return newTriple.position;
+    }
+
+    public static Integer addTriple(Object opcode, Object src) {
+        Triple newTriple = new Triple(opcode.toString(), src.toString(), "_");
+
+        triples.add(newTriple);
+
+        return newTriple.position;
+    }
+
+    public static Integer addTriple(Object opcode) {
+        Triple newTriple = new Triple(opcode.toString(), "_", "_");
+
+        triples.add(newTriple);
+        
+        return newTriple.position;
+    }
+
     @Override
     public void generate(FileWriter fileWriter) throws IOException {
-        fileWriter.write("TODO");
+        for (Triple triple : triples) {
+            fileWriter.write("[" + triple.position + "] " + "(" + triple.opcode + ", " + triple.src + ", " + triple.dest + ")\n");
+        }
     }
 }
